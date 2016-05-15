277ca97812a045b9c308443d0f71432fa6574493
Laurence Crevel <laurence@a-nounou.com>
2016-05-15 17:22:49 +0200

    Add a controller for the salads, and modify the views associated.

---
+++ app/controllers/salads_controller.rb
@@ -0,0 +1,42 @@
+class SaladsController < ApplicationController
+  before_action :set_salad, only: [:show, :edit, :update, :destroy]
+  def index
+    @salads = Salad.all
+  end
+
+  def show
+  end
+
+  def new
+    @salad = Salad.new
+  end
+
+  def create
+    @salad = Salad.new(salad_params)
+    @salad.save
+    redirect_to salad_path(@salad)
+  end
+
+  def edit
+  end
+
+  def update
+    @salad.update(salad_params)
+    redirect_to salad_path(@salad)
+  end
+
+  def destroy
+    @salad.destroy
+    redirect_to salads_path
+  end
+
+  private
+
+  def salad_params
+    params.require(:salad).permit(:name)
+  end
+
+  def set_salad
+    @salad = Salad.find(params[:id])
+  end
+end
